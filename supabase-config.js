65b0727830c648a2eb36ca98e0bd47f8acc48602
moss <frefoodie3@gmail.co.com>
2026-06-01 14:51:06 +0200

    Connect Tesfa Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.TESFA_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://txtbomachdygbshmuogg.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dGJvbWFjaGR5Z2JzaG11b2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDg4OTIsImV4cCI6MjA5NTg4NDg5Mn0._ft6OETfDsqhyC15YaFRCuGxxmpPEntdsKDMGubC_TQ",
   storageBucket: "gallery-previews"
 };
